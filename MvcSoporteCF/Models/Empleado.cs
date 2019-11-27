8f1b40b7d1b9ca1bf225af57e0d9aacd0a1857ab
DLGEJ-Entornos <delaguia19905@iesmarenostrum.com>
2019-11-27 16:09:26 +0100

    modelo de datos creado, creando vistas y controladores.

---
+++ MvcSoporteCF/Models/Empleado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcSoporteCF.Models
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre del empleado es un campo requerido.")]
+        public string Nombre { get; set; }
+        [Display(Name = "Correo electrónico")]
+        [EmailAddress]
+        public string Email { get; set; }
+        public virtual ICollection<Aviso> Avisos { get; set; }
+    }
+}
