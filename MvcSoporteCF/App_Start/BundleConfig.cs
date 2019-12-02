f4eae8dc05d79e87e1b1a41de720584d494ce7d0
DLGEJ-Entornos <delaguia19905@iesmarenostrum.com>
2019-12-02 13:25:06 +0100

    DataPicker-JQuery para crear , editar equipos añadidos

--- MvcSoporteCF/App_Start/BundleConfig.cs
+++ MvcSoporteCF/App_Start/BundleConfig.cs
@@ -17,7 +17,8 @@ namespace MvcSoporteCF
             // Añadido para Datapicker de JQueryUIHelpers.Mvc5
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
             "~/Scripts/jquery-ui-{version}.js",
-            "~/Scripts/jquery-ui.unobtrusive-{version}.js"));
+            "~/Scripts/jquery-ui.unobtrusive-{version}.js"));
+
 
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
             // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
@@ -35,7 +36,8 @@ namespace MvcSoporteCF
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
             "~/Content/themes/base/core.css",
             "~/Content/themes/base/datepicker.css",
-            "~/Content/themes/base/theme.css"));
+            "~/Content/themes/base/theme.css"));
+
         }
     }
 }
